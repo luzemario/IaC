84d8a639ac859aea0820686fb269ae1d51d5fcf8
Luzemário <luzemario@luzemario.net.br>
2025-03-25 21:53:58 -0300

    Update iac.sh

--- iac.sh
+++ iac.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 
+sudo su
+cd /
+
 echo "Configurando diretórios..."
 
-mkdir /publico
-mkdir /adm
-mkdir /ven
-mkdir /sec
+mkdir publico
+mkdir adm
+mkdir ven
+mkdir sec
 
-echo "Criando grupos..."
+echo "Adicionando grupos..."
 
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando usuários..."
+echo "Criando usuários administrativos..."
 
 useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
+echo "Criando usuários de vendas..."
+
 useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+echo "Criando usuários do secretariado..."
 
 useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
+echo "Atribuindo donos...."
+
+chown root:GRP_ADM adm
+chown root:GRP_VEN ven
+chown root:GRP_SEC sec
+
 echo "Ajustando permissões...."
 
-chown root:GRP_ADM /adm
-chown root:GRP_VEN /ven
-chown root:GRP_SEC /sec
-
-chmod 770 /adm
-chmod 770 /ven
-chmod 770 /sec
-chmod 777 /publico
+chmod 770 adm
+chmod 770 ven
+chmod 770 sec
+chmod 777 publico
 
 echo "Finalizado."
 
